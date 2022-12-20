e4d35110d6aadf3c46f5419c358b1026521601ac
Md Hridoy Bhuyan <hbhuyan@th-deg.de>
2022-12-20 11:31:51 +0100

    initial commit

---
+++ src/test/java/com/med4pan/cloudapp/CloudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.med4pan.cloudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
